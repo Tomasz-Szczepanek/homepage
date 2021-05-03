3412f710ecbec753be87f81ebba62e881f6de8a5
Tomasz Szczepanek <tomasz.szczepanek10@gmail.com>
2021-05-03 15:57:36 +0200

    function added to JavaScript

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
-console.log("Cześć!")
+{
 
-let header__headerButton = document.querySelector(".header__headerButton");
-let header__headers = document.querySelector(".header__headers");
+    const header__headerButton = document.querySelector(".header__headerButton");
+    const main__tableButton = document.querySelector(".main__tableButton");
 
-let main__tableButton = document.querySelector(".main__tableButton");
-let table = document.querySelector(".table");
+    const welcome = () => {
+        console.log("Witam wszystkich developerów, którzy tutaj zaglądają :)")
+    }
 
-header__headerButton.addEventListener("click", () => {
-    header__headers.classList.toggle("header__hiddenHeaders");
-    header__headerButton.innerText = header__headers.classList.contains("header__hiddenHeaders") ? "Pokaż nagłówek" : "Ukryj nagłówek";
-});
+    const onHideMainHeaderClick = () => {
+        const header__headers = document.querySelector(".header__headers");
+        header__headers.classList.toggle("header__hiddenHeaders");
+        header__headerButton.innerText = header__headers.classList.contains("header__hiddenHeaders") ? "Pokaż nagłówek" : "Ukryj nagłówek";
+    };
 
-main__tableButton.addEventListener("click", () => {
-    table.classList.toggle("main__hiddenTable")
-    main__tableButton.innerText = table.classList.contains("main__hiddenTable") ? "Pokaż tabele" : "Ukryj tabele";
-});
+    const onHideTableClick = () => {
+        const table = document.querySelector(".table");
+        table.classList.toggle("main__hiddenTable")
+        main__tableButton.innerText = table.classList.contains("main__hiddenTable") ? "Pokaż tabele" : "Ukryj tabele";
+    };
+
+    const init = () => {
+        header__headerButton.addEventListener("click", onHideMainHeaderClick);
+        main__tableButton.addEventListener("click", onHideTableClick);
+
+        welcome();
+    };
+
+    init();
+
+}
